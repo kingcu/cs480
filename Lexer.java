8c34f25742b8d3bb80644a0965d2c5d5adba29cd
Cullen King <kingcu@meeepc.(none)>
2009-01-07 20:59:23 -0800

    some more work on lexer, it is broken now, but i think i know what is wrong.  will fix later this evening

--- Lexer.java
+++ Lexer.java
@@ -8,6 +8,7 @@
 //
 
 import java.io.*;
+import java.util.regex.*;
 
 //
 //--------Lexer----------------
@@ -18,6 +19,13 @@ public class Lexer {
 	private String token;
 	private int tokenType;
 
+	private static final String COMMENT_REGEX = "\\{.\\}";
+	private static final String IDENT_REGEX = "[a-zA-Z]+\\w*";
+	private static final String KEYW_REGEX = "";
+	private static final String INT_REGEX = "";
+	private static final String REAL_REGEX = "";
+	private static final String STRING_REGEX = "";
+
 	public Lexer(Reader in) {
 		input = new PushbackReader(in);
 	}
@@ -25,9 +33,52 @@ public class Lexer {
 	private void skipWhiteSpace() throws ParseException, IOException {
 		// your code here
 	}
+
+	private int currentChar() throws ParseException {
+		int cc;
+
+		try {
+			cc = input.read();
+		} catch (IOException e) {
+			throw new ParseException(0);
+		}
+		return cc;
+	}	
 	
 	public void nextLex() throws ParseException {
-		// your code here
+		int c = currentChar();
+		System.out.println(c);
+		token = "";
+
+		//if(Character.isLetterOrDigit((char) c)) {
+			while(Character.isLetterOrDigit((char) c) && Pattern.matches("\\S", Character.toString((char)c))) {
+			//while(Character.isLetterOrDigit((char) c) && !Character.isWhitespace((char)c)) {
+				token = token + (char)c;
+				c = currentChar();
+			}
+
+			//if(!Character.isWhitespace((char)c)) {
+			if(Pattern.matches("\\S", Character.toString((char)c))) {
+				try {
+					input.unread(c);
+				} catch(IOException e) {
+					System.out.println("FUCKER UP OH NOS");
+				}
+			}
+			/*
+		} else if(Pattern.matches("\\S", Character.toString((char)c))) {
+		//} else if(!Character.isWhitespace((char)c)) {
+			token = token + (char)c;
+		}
+		*/
+
+		if(c == -1) { //end of input, toss to 7 so we can exit
+			tokenType = 7;
+		} else if(Pattern.matches(IDENT_REGEX, token)) {
+			tokenType = 1;
+		} else {
+			tokenType = 6;
+		}
 	}
 
 	static final int identifierToken = 1;
@@ -50,7 +101,7 @@ public class Lexer {
 		return tokenType == identifierToken;
 	}
 
-	public boolean match (String test) {
+	public boolean match(String test) {
 		return test.equals(token);
-		}
+	}
 }
